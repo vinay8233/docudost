e0ba62f99b02cec08cad910fd0ac80f99d599a72
vinay8233 <vinaypratap9660@gmail.com>
2025-11-26 18:44:20 +0530

    fix module path issues

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const apirouter = require("./routers/router");
 const cors = require("cors");
+const path = require("path");
 
 
 
@@ -13,12 +14,12 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", apirouter);
 app.use(express.urlencoded({ extended: true }));
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', 'my-project', 'build')));
-  app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '..', 'my-project', 'build', 'index.html'))
-  );
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '..', 'my-project', 'build')));
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.join(__dirname, '..', 'my-project', 'build', 'index.html'))
+//   );
+// }
 
 app.use("/uploads", express.static("uploads"));
 
